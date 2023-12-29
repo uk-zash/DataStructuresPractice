dd28acbafc9a6a94d1f48ccda89669629ef0c997
MD UMAR <142337730+uk-zash@users.noreply.github.com>
2023-12-29 20:49:32 +0530

    Update Peak_Index_in_MountainArray.java
    It's now a final solution from my side till now.
    Here input array should be of atleast length of 3 elements.
    And depends on the out put you can return the index or the peak element.

--- src/Peak_Index_in_MountainArray.java
+++ src/Peak_Index_in_MountainArray.java
@@ -3,7 +3,7 @@ public class Peak_Index_in_MountainArray {
         int n = nums.length;
         int r = n-1;
         int l = 0;
-        while (l<=r){
+        while (l<r){
             int mid =  l+ (r-l)/2;
             if (nums[mid]>nums[mid+1]&& nums[mid]>nums[mid-1]){
                 return mid;
@@ -15,7 +15,7 @@ public class Peak_Index_in_MountainArray {
                 r = mid+1;
             }
         }
-        return 1;
+        return n-1;
 
     }
 }
